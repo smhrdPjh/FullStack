1b9fb46dd10332d63b01e737f0b98015219c5b0a
smhrdPjh <you@example.com>
2022-12-07 17:51:32 +0900

    자동로그인, Firebase 연결, 즉시응답 확인

--- app/src/main/java/com/example/fullstackapplication/auth/LoginActivity.kt
+++ app/src/main/java/com/example/fullstackapplication/auth/LoginActivity.kt
@@ -1,14 +1,15 @@
 package com.example.fullstackapplication.auth
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.fullstackapplication.MainActivity
 import com.example.fullstackapplication.R
-import com.example.fullstackapplication.SplashActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -16,46 +17,57 @@ import com.google.firebase.ktx.Firebase
 class LoginActivity : AppCompatActivity() {
 
     //FirebaseAuth 선언
-    lateinit var auth : FirebaseAuth
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // 초기화화
-        auth=Firebase.auth
+        val sharedPreferences =
+            getSharedPreferences("autoLogin", Context.MODE_PRIVATE)
+        val loginId = sharedPreferences.getString("loginId", "")
+        val loginPw = sharedPreferences.getString("loginPw", "")
 
-        val etLoginPw =findViewById<EditText>(R.id.etLoginPw)
+        // FireabseAuth 초기화
+        auth = Firebase.auth
         val etLoginEmail = findViewById<EditText>(R.id.etLoginEmail)
+        val etLoginPw = findViewById<EditText>(R.id.etLoginPw)
         val btnLoginLogin = findViewById<Button>(R.id.btnLoginLogin)
 
-
-        // Login 버튼을 눌렀을 떄
+        etLoginEmail.setText(loginId)
+        etLoginPw.setText(loginPw)
+        // Login 버튼을 눌렀을 때
         btnLoginLogin.setOnClickListener {
             val email = etLoginEmail.text.toString()
             val pw = etLoginPw.text.toString()
 
-            // 로그인 할때 사용하는 메서드
-            auth.signInWithEmailAndPassword(email,pw)
-                .addOnCompleteListener(this){
-                    task ->
-                    if(task.isSuccessful){
-                        //로그인에 성공
-                        Toast.makeText(this,"로그인성공",Toast.LENGTH_SHORT).show()
-                  val intent = Intent(this,MainActivity::class.java)
+            auth.signInWithEmailAndPassword(email, pw)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 로그인에 성공
+                        Toast.makeText(
+                            this, "로그인 성공",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val editor = sharedPreferences.edit()
+                        editor.putString("loginId", email)
+                        editor.putString("loginPw", pw)
+                        editor.commit()
+
+                        val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
                         finish()
-                    }else{
+                    } else {
                         // 로그인에 실패
-                        Toast.makeText(this,"로그인실패",Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            this, "로그인 실패",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
 
-            Toast.makeText(this@LoginActivity,"$email, $pw", Toast.LENGTH_SHORT).show()
-
-
-
-
+            Toast.makeText(applicationContext, "email : $email, pw: $pw", Toast.LENGTH_SHORT).show()
+            Log.d("intrologin", "email : $email, pw: $pw")
+        }
     }
-}
 }
